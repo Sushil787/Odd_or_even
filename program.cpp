74fb334bd7a1c79628f8111c5fc18b6680ab55dc
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2020-08-29 22:51:20 +0545

    Create program.cpp

---
+++ program.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main(void){
+    int a;
+    std::cout<<"input the no you like: ";
+    std::cin>>a;
+    if(a%2==0){ 
+    std::cout<<"Even";
+    }
+    else{ 
+    std::cout<<"odd";
+    }
+}
+
